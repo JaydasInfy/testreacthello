967809b576d2df7e9e696742a9ce89e22eaec022
JaydasInfy <jayanta.das.2006@gmail.com>
2020-03-01 12:42:17 +0530

    1-March

---
+++ src/Component/testcomp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function testcomp() {
+  return (
+    <div>
+      <label>Full Name</label>
+      <img src={logo}></img>
+      <input type="text"></input>
+    </div>
+  );
+}
+
+export default testcomp;
